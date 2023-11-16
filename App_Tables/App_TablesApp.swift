4e6312ce39b66af56dde99354c86874d273f702e
Matthew Rispler <matthew.d.rispler@student.uts.edu.au>
2023-11-17 10:41:56 +1100

    Initial Commit

---
+++ App_Tables/App_TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_TablesApp.swift
+//  App_Tables
+//
+//  Created by Matthew Rispler on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct App_TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
