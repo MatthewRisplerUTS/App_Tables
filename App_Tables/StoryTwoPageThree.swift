086c434c870abef0cd7c91c05e9e73112a37d6ea
Nicolas Brex <flaminbanana711@gmail.com>
2023-11-21 11:01:35 +1100

    added cb2

---
+++ App_Tables/StoryTwoPageThree.swift
@@ -0,0 +1,18 @@
+//
+//  StoryTwoPageThree.swift
+//  App_Tables
+//
+//  Created by Nicolas Brex on 21/11/2023.
+//
+
+import SwiftUI
+
+struct StoryTwoPageThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryTwoPageThree()
+}
