4af41e9c77985432489d481c95677582a1f47008
Nicolas Brex <flaminbanana711@gmail.com>
2023-11-23 10:13:38 +1100

    Navlinks now open in fullscreen mostly

--- App_Tables/StoryOnePageOne.swift
+++ App_Tables/StoryOnePageOne.swift
@@ -12,54 +12,57 @@ struct SwiftUIView: View {
     @State private var selected = 0
     @State var showStoryTwoPageThree: Bool = false
     
+    
     var body: some View {
-        ZStack{
-            Rectangle()
-                .ignoresSafeArea()
-                .foregroundColor(Color(red: 0.93, green: 0.95, blue: 0.99))
-                .frame(width: 393, height:760)
-                
-            VStack{
-                Spacer()
-                HStack(alignment:.center){
-                    Spacer()
-                        .frame(width:30)
-                    Image(systemName: "person.crop.circle")
-                        .resizable()
-                        .frame(width:40,height:40)
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    Text("Victor Timely").bold()
-                        .font(.system(size: 34))
-                        .font(.headline)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                
-                HStack{
-                    Spacer()
-                        .frame(width:30)
-                    Text("I want to study...")
-                        .font(.title3)
-                    Spacer()
-                        .scaledToFit()
-                    Image(systemName:"line.3.horizontal.decrease.circle")
-                        .resizable()
-                        .frame(width:25,height:25)
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                        .frame(width:30)
-                }
+        NavigationView {
+            ZStack{
                 Rectangle()
-                    .foregroundColor(Color(red: 0.8, green: 0.82, blue: 0.99))
-                    .frame(height:2)
-                    .offset(y:8)
+                    .ignoresSafeArea()
+                    .foregroundColor(Color(red: 0.93, green: 0.95, blue: 0.99))
+                    .frame(width: 393, height:760)
                 
-                
-                
-                
-                NavigationStack {
+                VStack{
+                    Spacer()
+                    HStack(alignment:.center){
+                        Spacer()
+                            .frame(width:30)
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .frame(width:40,height:40)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        Text("Victor Timely").bold()
+                            .font(.system(size: 34))
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    
+                    HStack{
+                        Spacer()
+                            .frame(width:30)
+                        Text("I want to study...")
+                            .font(.title3)
+                        Spacer()
+                            .scaledToFit()
+                        Image(systemName:"line.3.horizontal.decrease.circle")
+                            .resizable()
+                            .frame(width:25,height:25)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        Spacer()
+                            .frame(width:30)
+                    }
+                    Rectangle()
+                        .foregroundColor(Color(red: 0.8, green: 0.82, blue: 0.99))
+                        .frame(height:2)
+                        .offset(y:8)
+                    
+                    
+                    
+                    
+                    
                     ScrollView {
+                        
                         // Listing 1
-                        NavigationLink(destination: StoryTwoPageThree(), label: {
+                        NavigationLink(destination: ContentView(), label: {
                             ZStack {
                                 RoundedRectangle(cornerRadius: 20)
                                     .frame(width:350,height:150)
@@ -100,8 +103,8 @@ struct SwiftUIView: View {
                                                 bottomTrailingRadius:20,
                                                 topTrailingRadius: 20))
                                 }}
-                            //    .fullScreenCover(isPresented: $showStoryTwoPageThree,content: {StoryTwoPageThree.init()})
                         })
+                        
                         
                         
                         // Listing 2
@@ -145,8 +148,8 @@ struct SwiftUIView: View {
                                                 bottomLeadingRadius: 0,
                                                 bottomTrailingRadius:20,
                                                 topTrailingRadius: 20))
-                                }}.fullScreenCover(isPresented: $showStoryTwoPageThree,content: {
-                                    StoryTwoPageThree.init()})})
+                                }}
+                        })
                         
                         
                         // Listing 3
@@ -190,8 +193,8 @@ struct SwiftUIView: View {
                                                 bottomLeadingRadius: 0,
                                                 bottomTrailingRadius:20,
                                                 topTrailingRadius: 20))
-                                }}.fullScreenCover(isPresented: $showStoryTwoPageThree,content: {
-                                    StoryTwoPageThree.init()})})
+                                }}
+                        })
                         
                         
                         // Listing 4
@@ -235,40 +238,36 @@ struct SwiftUIView: View {
                                                 bottomLeadingRadius: 0,
                                                 bottomTrailingRadius:20,
                                                 topTrailingRadius: 20))
-                                }}.fullScreenCover(isPresented: $showStoryTwoPageThree,content: {
-                                    StoryTwoPageThree.init()})})
-                    }}
+                                }}
+                        })
+                    }
+                    Rectangle()
+                        .foregroundColor(Color(red: 0.8, green: 0.82, blue: 0.99))
+                        .frame(height:2)
+                        .offset(y:-10)
+                    
+                    Picker("Choose course", selection: $selected) {
+                        Text("Join")
+                            .tag(0)
+                        Text("Create")
+                            .tag(1)
+                        
+                    }
+                    .pickerStyle(.segmented)
+                    .scaledToFill()
+                    .frame(width:350, height:30)
+                    
+                    Spacer()
+                        .frame(height:20)
+                }
                 
                 
-                Rectangle()
-                    .foregroundColor(Color(red: 0.8, green: 0.82, blue: 0.99))
-                    .frame(height:2)
-                    .offset(y:-10)
-                
-                Picker("Choose course", selection: $selected) {
-                            Text("Join")
-                                .tag(0)
-                            Text("Create")
-                                .tag(1)
-                                
-                        }
-                        .pickerStyle(.segmented)
-                        .scaledToFill()
-                        .frame(width:350, height:30)
-                
-                Spacer()
-                    .frame(height:20)
             }
             
             
-            
-            
-            
-            
-            
-            
-            
-        }}}
+        }
+        
+    }}
 
 #Preview {
     SwiftUIView()
