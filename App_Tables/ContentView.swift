3231e4ca9ee3bc15d9c521b30a4874aaf9dad9ae
Nicolas Brex <flaminbanana711@gmail.com>
2023-11-22 16:08:10 +1100

    Yay working buttons that do nothing

--- App_Tables/ContentView.swift
+++ App_Tables/ContentView.swift
@@ -72,66 +72,63 @@ struct ContentView: View {
         
         
         
+            Button {print("Ciao")} label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width:350,height:150)
+                            .foregroundColor(.white)
+                            .shadow(radius: 5)
+                        
+                        HStack{
+                            VStack{
+                                Spacer()
+                                    .frame(height:40)
+                                Text("Bananaas")
+                                    .scaledToFill()
+                                    .font(.system(size: 36))
+                                    .frame(width:180,alignment:.trailing)
+                                    .font(.headline)
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                Spacer()
+                                    .frame(height:20)
+                                Text("Building 11")
+                                    .frame(width:180,alignment:.trailing)
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                                Text("Until 5:00 PM")
+                                    .frame(width:180,alignment:.trailing)
+                                    .font(.caption)
+                                    .foregroundColor(.gray)
+                            }
+                            Rectangle()
+                                .frame(width:10,height:150)
+                                .offset(x:10)
+                                .foregroundColor(.purple)
+                            Image(.CB_11)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width:150,height:150)
+                                .clipShape(
+                                    .rect(
+                                        topLeadingRadius: 0,
+                                        bottomLeadingRadius: 0,
+                                        bottomTrailingRadius:20,
+                                        topTrailingRadius: 20
+                                    )
+                                )
+                        }}}
         
-        Button {
-                    print("Ciao")
-        } label: {
+            NavigationLink {StoryTwoPageThree()} label:{
             ZStack {
                 RoundedRectangle(cornerRadius: 20)
                     .frame(width:350,height:150)
                     .foregroundColor(.white)
                     .shadow(radius: 5)
-                
                 HStack{
                     VStack{
                         Spacer()
                             .frame(height:40)
-                        Text("Logistics")
-                            .scaledToFill()
-                            .font(.system(size: 36))
-                            .frame(width:180,alignment:.trailing)
-                            .font(.headline)
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                        Spacer()
-                            .frame(height:20)
-                        Text("Building 11")
-                            .frame(width:180,alignment:.trailing)
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                        Text("Until 5:00 PM")
-                            .frame(width:180,alignment:.trailing)
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                    }
-                    Rectangle()
-                        .frame(width:10,height:150)
-                        .offset(x:10)
-                        .foregroundColor(.purple)
-                    Image(.CB_11)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width:150,height:150)
-                        .clipShape(
-                            .rect(
-                                topLeadingRadius: 0,
-                                bottomLeadingRadius: 0,
-                                bottomTrailingRadius:20,
-                                topTrailingRadius: 20
-                            )
-                        )
-                }}}
-        
-        NavigationLink {StoryTwoPageThree()} label: {
-            ZStack {
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width:350,height:150)
-                    .foregroundColor(.white)
-                    .shadow(radius: 5)
-                HStack{
-                    VStack{
-                        Spacer()
-                            .frame(height:40)
-                        Text("Logistics")
+                        Text("Rockets")
                             .scaledToFill()
                             .font(.system(size: 36))
                             .frame(width:180,alignment:.trailing)
--- App_Tables/StoryOnePageOne.swift
+++ App_Tables/StoryOnePageOne.swift
@@ -51,59 +51,56 @@ struct SwiftUIView: View {
                     .foregroundColor(Color(red: 0.8, green: 0.82, blue: 0.99))
                     .frame(height:2)
                     .offset(y:8)
-                
-                
                 ScrollView {
-                    VStack{
-                        NavigationLink {ContentView()} label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .frame(width:350,height:150)
-                                    .foregroundColor(.white)
-                                    .shadow(radius: 5)
-                                HStack{
-                                    VStack{
-                                        Spacer()
-                                            .frame(height:40)
-                                        Text("Logistics")
-                                            .scaledToFill()
-                                            .font(.system(size: 36))
-                                            .frame(width:180,alignment:.trailing)
-                                            .font(.headline)
-                                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                                        Spacer()
-                                            .frame(height:20)
-                                        Text("Building 11")
-                                            .frame(width:180,alignment:.trailing)
-                                            .font(.caption)
-                                            .foregroundColor(.gray)
-                                        Text("Until 5:00 PM")
-                                            .frame(width:180,alignment:.trailing)
-                                            .font(.caption)
-                                            .foregroundColor(.gray)
-                                    }
-                                    Rectangle()
-                                        .frame(width:10,height:150)
-                                        .offset(x:10)
-                                        .foregroundColor(.purple)
-                                    Image(.CB_11)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width:150,height:150)
-                                        .clipShape(
-                                            .rect(
-                                                topLeadingRadius: 0,
-                                                bottomLeadingRadius: 0,
-                                                bottomTrailingRadius:20,
-                                                topTrailingRadius: 20
-                                            )
-                                        )
+                    
+                    
+                    Button {print("Ciao")} label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width:350,height:150)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                            HStack{
+                                VStack{
+                                    Spacer()
+                                        .frame(height:40)
+                                    Text("Logistics")
+                                        .scaledToFill()
+                                        .font(.system(size: 36))
+                                        .frame(width:180,alignment:.trailing)
+                                        .font(.headline)
+                                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                    Spacer()
+                                        .frame(height:20)
+                                    Text("Building 11")
+                                        .frame(width:180,alignment:.trailing)
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                    Text("Until 5:00 PM")
+                                        .frame(width:180,alignment:.trailing)
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
                                 }
-                            }}
-                        .listRowSeparator(.hidden)
-                        .listRowBackground(Color(red: 0.93, green: 0.95, blue: 0.99))
-                        
-                        
+                                Rectangle()
+                                    .frame(width:10,height:150)
+                                    .offset(x:10)
+                                    .foregroundColor(.purple)
+                                Image(.CB_11)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:150,height:150)
+                                    .clipShape(
+                                        .rect(
+                                            topLeadingRadius: 0,
+                                            bottomLeadingRadius: 0,
+                                            bottomTrailingRadius:20,
+                                            topTrailingRadius: 20
+                                        )
+                                    )
+                            }}}
+                    
+                    
+                    Button {print("Ciao")} label:{
                         ZStack {
                             RoundedRectangle(cornerRadius: 20)
                                 .frame(width:350,height:150)
@@ -149,9 +146,9 @@ struct SwiftUIView: View {
                                     )
                             }
                             
-                        }.listRowSeparator(.hidden)
-                            .listRowBackground(Color(red: 0.93, green: 0.95, blue: 0.99))
-                        
+                        }}
+                    
+                    Button {print("Ciao")} label:{
                         ZStack {
                             RoundedRectangle(cornerRadius: 20)
                                 .frame(width:350,height:150)
@@ -198,9 +195,9 @@ struct SwiftUIView: View {
                             }
                             
                             
-                        }.listRowSeparator(.hidden)
-                            .listRowBackground(Color(red: 0.93, green: 0.95, blue: 0.99))
-                        
+                        }}
+                    
+                    Button {print("Ciao")} label:{
                         ZStack {
                             RoundedRectangle(cornerRadius: 20)
                                 .frame(width:350,height:150)
@@ -244,20 +241,10 @@ struct SwiftUIView: View {
                                             topTrailingRadius: 20
                                         )
                                     )
-                            }
-                            
-                        }.listRowSeparator(.hidden)
-                            .listRowBackground(Color(red: 0.93, green: 0.95, blue: 0.99))
-                        
-                        
-                    }}
-                .scrollContentBackground(.hidden)
-                .frame( maxWidth: .infinity)
-                .edgesIgnoringSafeArea(.all)
-                .listStyle(GroupedListStyle())
-                
-                
-                
+                            }}}
+                    
+                    
+                }
                 
                 
                 Rectangle()
